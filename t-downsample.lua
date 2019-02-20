2456c29485bafd4d68cfebba6da4cff01e480f13
Marco Patzer <mail@homerow.info>
2019-02-20 20:47:13 +0100

    Incorporate Peter's changes

--- t-downsample.lua
+++ t-downsample.lua
@@ -1,51 +1,46 @@
 if not modules then modules = { } end modules ['t-downsample'] = {
-version   = 1.004,
+version   = 1.005,
 comment   = "companion to grph-inc.mkiv",
 author    = "Peter Münster, Marco Patzer",
 copyright = "PRAGMA ADE / ConTeXt Development Team",
 license   = "see context related readme files"
 }
 
+local myself = "downsample"
 local format = string.format
+local report = logs.report
 
 local function sample_down(oldname, newname, resolution)
 	local request = figures.current().request
-	local width = request.width
-	local height = request.height
+	local width   = request.width
+	local height  = request.height
 	if resolution == "" or (not width and not height) then
-		-- can work only if width and/or height ist set
-		logs.report(string.format("Nothing to do: %s, %s, %s", oldname, newname, resolution))
+		report(myself, format("Nothing to do: %s, %s, %s",
+			oldname, newname, resolution))
+		file.copy(oldname, newname)
 		return
 	end
-	local inch = 72.27
+	local inch  = 72.27 * 65536
 	local image = img.scan{filename = oldname}
-	local xy = image.xsize / image.ysize
+	local xy    = image.xsize / image.ysize
 	if not width then
-		width = height * xy / 65536
+		width = height * xy
 	end
 	if not height then
-		height = width / xy / 65536
+		height = width / xy
 	end
 	local xsize = math.floor(resolution * width / inch)
 	local ysize = math.floor(resolution * height / inch)
 	if xsize < image.xsize or ysize < image.ysize then
-		local suffix = oldname:match("[^.]+$")
-		local s
-		if suffix == "jpg" then
-			s = format("gm convert -strip -resize '%dx%d>' %s jpg:%s",
-				xsize, ysize, oldname, newname)
-		else if suffix == "png" then
-			s = format("gm convert -strip -resize '%dx%d>' %s png:%s",
-				xsize, ysize, oldname, newname)
-		else if suffix == "gif" then
-			s = format("gm convert -strip -resize '%dx%d>' %s gif:%s",
-				xsize, ysize, oldname, newname)
-		end end end
-		logs.report("Conversion: " .. s)
+		local s = format("gm convert -strip -resize '%dx%d>' %s %s",
+			xsize, ysize, oldname, newname)
+		report(myself, "Conversion: " .. s)
 		os.execute(s)
 	else
-		logs.report(print.format("Nothing to do: %s, %s, %s", oldname, newname, resolution))
-		logs.report(print.format("xsize = %d, ysize = %d", xsize, ysize))
+		report(myself, format("Nothing to do: %s, %s, %s",
+			oldname, newname, resolution))
+		report(myself, format("xsize = %d, ysize = %d", xsize, ysize))
+		file.copy(oldname, newname)
 	end
 end
 
